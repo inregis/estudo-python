a742be25f7bcca333f10b6d1ff448d782a0f57ec
inregis <inregis1@gmail.com>
2026-04-24 22:05:35 -0300

    Atualização 2.1

--- 13 - APIs Assíncronas com FastAPI/dio-blog/main.py
+++ 13 - APIs Assíncronas com FastAPI/dio-blog/main.py
@@ -1,8 +1,87 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+
+from src.controllers import auth, post
+from src.database import database
+from src.exceptions import NotFoundPostError
 
 
-@app.get("/")
-def read_():
-    return {"message": "Hello Word"}
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await database.connect()
+    yield
+    await database.disconnect()
+
+
+tags_metadata = [
+    {
+        "name": "auth",
+        "description": "Operações para autenticação",
+    },
+    {
+        "name": "post",
+        "description": "Operações para manter posts.",
+        "externalDocs": {
+            "description": "Documentação externa para Posts.api",
+            "url": "https://post-api.com/",
+        },
+    },
+]
+
+servers = [
+    {"url": "http://localhost:8000", "description": "Ambiente de desenvolvimento"},
+    {
+        "url": "https://dio-blog-fastapi.onrender.com",
+        "description": "Ambiente de produção",
+    },
+]
+
+
+app = FastAPI(
+    title="DIO blog API",
+    version="1.2.0",
+    summary="API para blog pessoal.",
+    description="""
+DIO blog API ajuda você a criar seu blog pessoal. 🚀
+
+## Posts
+
+Você será capaz de fazer:
+
+* **Criar posts**.
+* **Recuperar posts**.
+* **Recuperar posts por ID**.
+* **Atualizar posts**.
+* **Excluir posts**.
+* **Limitar quantidade de posts diários** (_not implemented_).
+                """,
+    openapi_tags=tags_metadata,
+    servers=servers,
+    redoc_url=None,
+    # openapi_url=None, # disable docs
+    lifespan=lifespan,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth.router, tags=["auth"])
+app.include_router(post.router, tags=["post"])
+
+
+@app.exception_handler(NotFoundPostError)
+async def not_found_post_exception_handler(request: Request, exc: NotFoundPostError):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.message},
+    )
+
+
