8d902f5c42bebe5d11b9d88442efa98aca73cf15
inregis <inregis1@gmail.com>
2026-05-08 10:05:19 -0300

    Atualização 2.7

---
+++ 01 - Estrutura de dados/03 - Conjutos/00_declarando_conjuntos.py
@@ -0,0 +1,11 @@
+numeros =set([1,2,3,1,3,4])
+print(numeros)
+
+letras = set("abacaxi")
+print(letras)
+
+carros = {"gol", "celta", "palio", "gol"}
+print(carros)
+
+linguagens = {"python", "java", "python"}
+print(linguagens)
